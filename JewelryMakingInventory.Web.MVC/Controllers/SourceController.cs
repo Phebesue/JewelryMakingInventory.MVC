48b00becf82775905d51b567682c65bd16fb1c4d
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-06-30 17:09:41 -0400

    Create all Controllers. Build out Source Controller full CRUD/ PGPD. Update Index on shared layout

---
+++ JewelryMakingInventory.Web.MVC/Controllers/SourceController.cs
@@ -0,0 +1,111 @@
+﻿using JewelryMaking.Models;
+using JewelryMaking.Services;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using System.Web.UI.WebControls;
+
+namespace JewelryMakingInventory.Web.MVC.Controllers
+{
+    public class SourceController : Controller
+    {
+        // GET: Source/Index
+        public ActionResult Index()
+        {
+            var model = new SourceListAll[0];
+            return View(model);
+        }
+        //GET: Source/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Source/Create
+        [HttpPost]
+        public ActionResult Create(SourceCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var service = new SourceService();
+
+            if (service.CreateSource(model))
+            {
+                TempData["SaveResult"] = "Your source was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Source could not be created.");
+
+            return View(model);
+        }
+        //GET: Source/Details
+        public ActionResult Details(int id)
+        {
+            var svc = new SourceService();
+            var model = svc.GetSourceById(id);
+
+            return View(model);
+        }
+        //GET: Source/Edit
+        public ActionResult Edit(int id)
+        {
+            var service = new SourceService();
+            var detail = service.GetSourceById(id);
+            var model = new SourceEdit
+            {
+                SourceId = detail.SourceId,
+                Name = detail.Name,
+                WebSite = detail.WebSite,
+                ShowOrLocation = detail.ShowOrLocation,
+                Address = detail.Address,
+                City = detail.City,
+                State = detail.State,
+                ZipCode = detail.ZipCode,
+                Note = detail.Note
+            };
+            return View(model);
+        }
+        //POST: Source/Edit
+        [HttpPost]
+        public ActionResult Edit(int id, SourceEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if (model.SourceId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = new SourceService();
+            if (service.UpdateSource(model))
+            {
+                TempData["SaveResult"] = "Your source was updated.";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Your source could not be updated.");
+            return View(model);
+        }
+        // GET: Source/ Delete
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = new SourceService();
+            var model = svc.GetSourceById(id);
+
+            return View(model);
+        }
+        // POST: Source Delete
+        [HttpPost]
+        [ActionName("Delete")]
+            public ActionResult DeletePost (int id)
+        {
+            var service = new SourceService();
+            service.DeleteSource(id);
+            TempData["SaveResult"] = "Your note was deleted";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ JewelryMakingInventory.Web.MVC/Controllers/StringingMaterialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JewelryMakingInventory.Web.MVC.Controllers
+{
+    public class StringingMaterialController : Controller
+    {
+        // GET: StringingMaterial
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
